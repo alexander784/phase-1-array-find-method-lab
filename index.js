3955fe6534422da7dd369818652e5aacb15ba594
alexander <alexandernyaga1999@gmail.com>
2023-09-30 17:55:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(record) {
+    const winningRecord = record.find(record => record.result === "W");
+    if(winningRecord) {
+        return winningRecord.year;
+    }else{
+        return undefined;
+    }
+}
